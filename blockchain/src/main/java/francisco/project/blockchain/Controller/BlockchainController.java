2b25b7f0d855cd18c9a0022f86ac84d7dd89e1f2
Francisco-Teixeirax86 <franjateixeira@gmail.com>
2025-02-17 21:13:07 +0000

    Introduced servers and peers
    Introduced servers and peers that communicate and broadcast each other's server as a way of keeping data integrety

--- blockchain/src/main/java/francisco/project/blockchain/Controller/BlockchainController.java
+++ blockchain/src/main/java/francisco/project/blockchain/Controller/BlockchainController.java
@@ -3,21 +3,28 @@ package francisco.project.blockchain.Controller;
 import francisco.project.blockchain.components.Block;
 import francisco.project.blockchain.components.Blockchain;
 import francisco.project.blockchain.components.Transaction;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @RestController
 @RequestMapping("/api")
 public class BlockchainController {
     private final Blockchain blockchain;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public BlockchainController(Blockchain blockchain) {
+    public BlockchainController(Blockchain blockchain, RestTemplate restTemplate) {
         this.blockchain = blockchain;
+        this.restTemplate = restTemplate;
     }
 
     @PostMapping("/transaction")
@@ -46,5 +53,33 @@ public class BlockchainController {
     public ResponseEntity<Double> getBalance(@PathVariable String address) {
         double balance = blockchain.getBalance(address);
         return ResponseEntity.ok(balance);
+    }
+
+    @PostMapping("/chain")
+    public ResponseEntity<String> receiveChain(@RequestBody List<Block> newChain) {
+        return blockchain.replaceChain(newChain) ? ResponseEntity.ok("Chain replaced!") : ResponseEntity.badRequest().body("Chain not replaced");
+    }
+
+    @PostMapping("/peers")
+    public ResponseEntity<String> registerPeer(@RequestBody String peer) {
+        this.blockchain.addPeer(peer);
+        return ResponseEntity.ok("Peer added successfully");
+    }
+
+    @GetMapping("/peers")
+    public ResponseEntity<Set<String>> getPeers() {
+        return ResponseEntity.ok(this.blockchain.getPeers());
+    }
+
+    @PostConstruct
+    public void syncChains() {
+        for (String peer : this.blockchain.getPeers()) {
+            try {
+                List<Block> peerChain = restTemplate.getForObject(peer + "/api/chain", List.class);
+                blockchain.replaceChain(peerChain);
+            } catch (Exception e) {
+                System.out.println("Failed to sync chains: " + peer);
+            }
+        }
     }
 }
